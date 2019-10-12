7580ca1ac909166837789d9d83b9eb6d24ffa2da
kriale <55289621+kriale@users.noreply.github.com>
2019-10-12 14:21:23 +0300

    Create Lab2

---
+++ Labs/Lab2/app/src/main/java/edu/kriale/lab2/MainActivity.java
@@ -0,0 +1,43 @@
+package edu.kriale.lab2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private static final String QUESTION_EXTRA_KEY = "question";
+    private static final String ANSWER_EXTRA_KEY = "answer";
+    private static final int SEND_QUESTION_ACTION = 10;
+    private static final String EMPTY_TEXT = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+
+    public void onClick_SendQuestion(View view) {
+        EditText QuestionEditText = findViewById( R.id.questionText );
+        String question = QuestionEditText.getText().toString();
+
+        Intent questionIntent = new Intent( this, SecondActivity.class );
+        questionIntent.putExtra( QUESTION_EXTRA_KEY, question );
+        startActivityForResult( questionIntent, SEND_QUESTION_ACTION );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == SEND_QUESTION_ACTION) {
+            String answer = data.getStringExtra( ANSWER_EXTRA_KEY );
+            TextView receivedAnswerText = findViewById( R.id.receivedAnswerText );
+            if (answer != null) {
+                receivedAnswerText.setText( answer );
+            } else {
+                receivedAnswerText.setText( EMPTY_TEXT ); // stub
+            }
+        }
+    }
+}
---
+++ Labs/Lab2/app/src/main/java/edu/kriale/lab2/SecondActivity.java
@@ -0,0 +1,47 @@
+package edu.kriale.lab2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+    private static final String QUESTION_EXTRA_KEY = "question";
+    private static final String ANSWER_EXTRA_KEY = "answer";
+    private static final String EMPTY_TEXT = "";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_second );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        TextView questionTextView = findViewById( R.id.receivedQuestionText );
+        Intent receivedIntent = getIntent();
+        String receivedQuestion = receivedIntent.getStringExtra( QUESTION_EXTRA_KEY );
+        if (receivedQuestion  != null) {
+            questionTextView.setText( receivedQuestion );
+        } else {
+            questionTextView.setText( EMPTY_TEXT ); // stub
+        }
+    }
+
+    public void onClick_SendAnswer(View view) {
+        EditText AnswerEditText = findViewById( R.id.answerText );
+        String answer = AnswerEditText.getText().toString();
+
+        Intent answerIntent = new Intent();
+        answerIntent.putExtra( ANSWER_EXTRA_KEY, answer );
+
+        setResult(Activity.RESULT_OK, answerIntent);
+        finish();
+    }
+
+}
